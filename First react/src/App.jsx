5b07c0f1adade3e951d36b7e21f8fdc3f9215523
prajwal896 <prajwaladaki999@gmail.com>
2026-05-21 22:49:45 +0530

    half done of card roject

--- First react/src/App.jsx
+++ First react/src/App.jsx
@@ -1,10 +1,19 @@
-import Navbar from "./navbar"
+import React from 'react'
+import { useEffect} from 'react'
+import Card from "./card"
+import { use } from 'react'
 
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+}, [])
 
 function App() {
   return (
     <>
-      <Navbar homeText="lauda" aboutText="lassan" contactText="Contact" />
+      <Card />
     </>
   )
 }
---
+++ First react/src/card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './card.css'
+
+const Card = () => {
+
+    return (
+    <div className="card">
+    <ul>
+        <li style={{paddingRight: '10vh'}}>userid</li>
+        <li style={{paddingRight: '10vh'}}>id</li>
+        <li style={{paddingRight: '50vh'}}>title</li>
+        <li style={{paddingRight: '50vh'}}>body</li>
+    </ul>
+    </div>
+)
+}
+
+export default Card
